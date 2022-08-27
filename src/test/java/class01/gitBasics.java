39d2f38d1e4dd3b2b820f315b4f5e3b3b84ea5bf
user <ozoda.imomkulova@gmail.com>
2022-08-27 19:39:12 +0500

    three line in code

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -3,5 +3,8 @@ package class01;
 public class gitBasics {
     public static void main(String[]args){
         System.out.println("today is not  beautiful day");
+        System.out.println("today is not  beautiful day");
+        System.out.println("today is not  beautiful day");
+
     }
 }
